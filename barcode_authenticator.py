8119abc273c09caf87de663ad8f59001c48ea7b3
bharat-rajani <bharat.ramrajani@gmail.com>
2018-03-30 13:31:01 +0530

    Files updated

---
+++ barcode_authenticator.py
@@ -0,0 +1,153 @@
+from random import randint
+import pprint
+import sys
+import pickle
+import os
+from termcolor import colored, cprint
+
+#no_dupes = [x for n, x in enumerate(rand_list) if x not in rand_list[:n]]
+#if output of below statement is less than 5000 then there exists a duplicate itemin rand_list.
+#print("Non duplicate numbers....",len(no_dupes))
+
+
+def allot_pass(randict):
+	name=input("Enter the name of person whom you want to allot the pass : ")
+	start=int(input("Input starting serial number "))
+	end=int(input("Input ending serial number "))
+
+	for key,values in randict.items():
+		
+		if values[1] in range(start,end+1):
+			if randict[key][2]=='dummy-name':
+				randict[key][2]=name
+				print("Pass {0} is alloted to {1}".format(randict[key][1],randict[key][2]))
+
+			else :				
+				confirm = input("You still want to allot the pass to %s"%name,"[yes/no]")
+				if confirm=='yes' or confirm=='y':
+					randict[key][2]=name
+				elif confirm=='no' or confirm=='n':
+					continue
+				else :
+					print("Wrong option ,continuing without alloting the pass {} to {}".format(randict[key][1],name))
+	
+	save_dict(randict)
+	print("Done.")
+
+
+def default_dict():
+	with open("barcode-data-368-499.txt","r") as bfile:
+		bfile_list=bfile.read().splitlines()
+	if len(bfile_list)==len(range(368,500)):
+		randict = {key:[0,num,"dummy-name"] for num,key in zip(range(368,500),bfile_list)}
+	else:
+		print("length not same")
+	return randict
+
+
+
+def save_dict(data):
+	if not os.path.exists('bardata'):
+		os.makedirs('bardata')
+	with open('bardata/bardata'+'.pkl','wb') as bd:
+		pickle.dump(data,bd,pickle.HIGHEST_PROTOCOL)
+
+
+def load_dict():
+	with open('bardata/bardata.pkl','rb') as bd:
+		return pickle.load(bd)
+
+def menu():
+	print("\n"*3)
+	w="(without quotes)."
+	print("[1]To start scanning from scratch type 'start'",w)
+	print("[2]To start scanning from saved data type 'load_data'",w)
+	print("[3]To stop scanning type 'stop'",w)
+	print("[4]To allot pass type 'allot'",w)
+	print("[5]To repeat this menu type 'help'",w)
+	print("\n"*3)
+
+def scan(randict):
+	print("\n"*2)
+	print("Scanning Started".center(50,"="))
+	print("\n"*2)
+	while 1:
+
+		print("\n"*2)
+		line = sys.stdin.readline().rstrip()
+		if line in randict.keys():
+			print("PASS NUMBER ",randict[line][1])
+			if randict[line][0] == 0:
+				
+				print(colored("PASS SCANNED.", 'green',attrs=['reverse']))
+			
+			elif randict[line][0] > 0:
+				print("Pass already scanned.")
+				cprint('Duplicate pass!', 'red', 'on_white',attrs=['reverse','blink'])
+				print("Scanned :",randict[line][0]+1,"times")
+		
+			print("PASS ALLOTTED BY ",colored(randict[line][2], 'yellow',attrs=['reverse']))
+			randict[line][0] += 1
+			save_dict(randict)
+
+		elif line=='stop' or line=='3':
+			break
+
+		elif line=='help' or line=='2':
+			menu()
+
+		elif line=="allot" or line=='3':
+			print("First you have to stop the scan in progress.(press 3 to stop")
+
+		else:
+			print("Barcode {} not found.".format(line))
+
+		
+def main():
+	#print(default_dict(),sep="\n",end='\n')
+
+	#for keys,val in randict.items():
+	#	print(keys,val)
+	print("\n"*3)
+	print("Barcode Scanner".center(8,"="))
+
+	for i in range(100):
+		menu()
+		command = sys.stdin.readline().rstrip()
+		if command == "start" or command=='1':
+			randict = default_dict()
+			scan(randict)
+		
+		elif command == "load_data" or command=='2':
+			randict = load_dict()
+			scan(randict)
+
+		elif command == "stop" or command=='3':
+			break
+
+		elif command == 'help' or command=='5':
+			menu()
+
+		elif command == 'allot' or command=='4':
+			print("You want to allot pass in saved data ?")
+			y=input("yes OR no ")
+			if y=="y" or y=="yes":
+				randict=load_dict()
+				allot_pass(randict)
+
+			elif y=='no' or y=='n':
+				print("names entered will be in default database")
+				randict=default_dict()
+				allot_pass(randict)
+
+			
+
+		else:
+			print("Bad option.")
+			continue
+
+
+
+if __name__=="__main__":
+	main()
+
---
+++ barcoded_passes.py
@@ -0,0 +1,121 @@
+import barcode
+from barcode.writer import ImageWriter
+from random import randint
+import os
+from PIL import Image, ImageFont ,ImageDraw
+import pickle
+
+#RANDOM NUMBERS GENERATOR
+random_number_digits = lambda x : randint(10**(x-1),(10**x)-1)
+#random number file
+rand_list = sorted([random_number_digits(13) for x in range(5000)])
+
+
+data = {x:[0,"%04d"%y] for x,y in zip(rand_list,range(1,5001))}
+#print(i for i in data.items())
+
+os.makedirs('obj')
+
+def save_obj(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open('obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+save_obj(data,"data")
+w=load_obj('data')
+for key,val in w.items():
+	print(key,val)
+
+
+
+
+rfile = open("rfile.txt","w")
+
+main_dir = '/home/bharat/code'
+
+#directory for barcode images
+barcode_images = os.path.join(main_dir,'barcode_images')
+if not os.path.exists(barcode_images):
+	os.makedirs(barcode_images)
+
+#directory for pass images
+pass_images = os.path.join(main_dir,'pass_images')
+if not os.path.exists(pass_images):
+	os.makedirs(pass_images)
+
+
+#BARCODE WRITER OPTIONS AND BARCODE CLASS
+options = dict(module_width=0.4,font_size=10,module_height=9,text_distance=2)
+EAN = barcode.get_barcode_class('ean13')
+
+#Change dir
+font = ImageFont.truetype("DejaVuSans-Bold.ttf", 18,encoding="unic")
+
+
+
+
+
+
+
+
+for i in range(5000):
+
+	os.chdir(main_dir)
+
+	barcode_number=str(rand_list[i])
+	#barcode_number="000000000"+"%4d"%i
+	print(barcode_number)
+	print(type(barcode_number))
+	
+	rfile.write(barcode_number+"\n")
+
+	ean = EAN(barcode_number,writer=ImageWriter())
+	#saving barcode images in barcode_images directory 
+	os.chdir(barcode_images)
+	barcode_img = 'ean13_barcode_%s'%(i+1)
+	print("[*] Barcode {} saved in {} ".format(i+1,"barcode_images"))
+
+	barcode = ean.save(barcode_img,options)
+	
+
+	img_bcode = Image.open(barcode_img+'.png', 'r')
+	img_w, img_h = img_bcode.size
+
+	#print(img_w,img_h)
+	#background = Image.new('RGBA', (1440, 900), (255, 255, 255, 255))
+
+	angle=90
+	rot = img_bcode.rotate( angle, expand=1 )
+
+	youthfest_pass = Image.open('/home/bharat/TYFPASSES.jpg','r')
+
+	bg_w, bg_h = youthfest_pass.size
+	#print("youthfest pass size ",youthfest_pass.size)
+	offset = ((bg_w - (img_h + 53)), (bg_h - img_w)//2)
+
+	#offset1 = (int(str(y) for y in offset:)
+	#offset1 = [int(y) for y in offset]
+	#print(offset)
+	youthfest_pass.paste(rot, offset)
+	serial_num = "%04d"%(i+1)
+	draw = ImageDraw.Draw(youthfest_pass)
+	txt = draw.text((60, 0),serial_num,(255,255,255),font=font)
+	#saving pass in pass images directory
+	os.chdir(pass_images)
+	youthfest_pass.save('pass%s.png'%(i+1))
+	print("[*] Pass {} saved in {} ".format(i+1,"pass_images"))
+
+	#closing all the pillow images
+	img_bcode.close()
+	youthfest_pass.close()
+
+print("All numbers are written at rfile.txt")
+
+#options = dict(compress=True)
+	
+
+	
